f405cf7a1e3c3489a7e7ae0ec5924ae96cac951f
Noifla01 <alfiosuta49@gmail.com>
2024-03-07 15:45:00 +0100

    Navbar inserita e h1 inserito

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    
+    <div class="container">
+        <div class="row justify-content-center text-center bg-secondary py-5">
+            <div class="col-12 col-md-6">
+                <h1 class="display-5 fw-bold"> The Aulab Post </h1>
+            </div>
+        </div>
+    </div>
+    
+</x-layout>
