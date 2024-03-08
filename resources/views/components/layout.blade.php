fdf115ad29bf83074b59cc1b08d79bd3b81abc3d
Jasmine <jasmine.nico94@icloud.com>
2024-03-08 16:04:37 +0100

    articolo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    {{-- <link rel="shortcut icon" href="/public/media/favicon1.png" type="image/x-icon"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
