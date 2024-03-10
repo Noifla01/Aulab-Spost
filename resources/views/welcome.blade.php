1d9279062a0e184fa114d187ad6b0f6b978a9c24
Jasmine <jasmine.nico94@icloud.com>
2024-03-10 11:32:40 +0100

    create card sulla home da completare

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,4 +14,23 @@
         </div>
     @endif
 
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            @foreach ($articles as $article)
+            <div class="col-12 col-md-3">
+                <x-card
+            title="{{ $article->title}}"
+            subtitle="{{ $article->subtitle}}"
+            image="{{ $article->image}}"
+            category="{{ $article->category}}"
+            data="{{ $article->created_at->format('d/m/Y')}}"
+            user="{{ $article->user->name}}"
+            url="#"
+            />
+            </div>
+            
+            @endforeach
+        </div>
+    </div>
+
 </x-layout>
