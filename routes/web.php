0d007c1ff1cf97a28207253da70af558a33b5b0d
Jasmine <jasmine.nico94@icloud.com>
2024-03-12 15:38:21 +0100

    risolto merge

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/article/create', [ArticleController::class,'create'])->name('articl
 
 Route::post('/article/store', [ArticleController::class,'store'])->name('article.store');
 
-
+Route::get('/article/index', [ArticleController::class, 'index'])->name('article.index');    
 
 Route::get('/article/show/{article}', [ArticleController::class, 'show'])->name('article.show');
 
