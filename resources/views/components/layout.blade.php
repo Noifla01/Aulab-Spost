dd3fea95f2a266397de53b8830c4ece00f6acc0f
Jasmine <jasmine.nico94@icloud.com>
2024-03-14 15:22:40 +0100

    ultimo agg con vista di ultimi 6 articoli

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    {{-- <link rel="shortcut icon" href="/public/media/favicon1.png" type="image/x-icon"> --}}
+    <link rel="shortcut icon" href="/public/media/favicon1.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -10,7 +10,7 @@
     <title class="">The Aulab sPost</title>
 </head>
 <body>
-    <x-navbar />
+    <x-navbar/>
 {{$slot}}
 
 
